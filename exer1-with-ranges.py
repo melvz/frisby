b11311c26887e2d06331ae29c38bf3dfe2d0acc2
Melvz <chuar@ph.ibm.com>
2018-04-10 12:40:21 +0800

    Create exer1-with-ranges.py

---
+++ exer1-with-ranges.py
@@ -0,0 +1,45 @@
+
+import math
+
+
+
+P = input("enter P: ")
+
+
+
+
+
+if P < -50:
+
+	print "Out of Range from negative"
+
+
+
+if P in range(-49, 0):
+
+	print "Negative Range"
+
+
+
+
+
+if P in range(0, 50):
+
+	print "positive Range"
+
+
+
+if P > 50:
+
+	print "Out of Rang from positive"
+
+
+
+if P == 50:
+
+	print "Border"
+
+
+if P == -50:
+
+	print "Border"
