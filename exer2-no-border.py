5b1a74d86f0c870eb87abbb4f0d6967b030b7d47
Melvz <chuar@ph.ibm.com>
2018-04-10 12:38:38 +0800

    Create exer2-no-border.py

---
+++ exer2-no-border.py
@@ -0,0 +1,46 @@
+import math
+
+
+
+P = input("enter P: ")
+
+
+
+
+
+if P < -50:
+
+	print "Out of Range from negative"
+
+
+
+if P < 0 and P > -50:
+
+	print "Negative Range"
+
+
+
+
+
+if P > 0 and P < 50:
+
+	print "positive Range"
+
+
+
+if P > 50:
+
+
+        print "Out of Rang from positive"
+
+
+
+
+if P == 50:
+
+		print "Border"
+
+
+if P == -50:
+
+		print "Border"
