9b021cd88e5c1cc2e0212cb8c6abd23ce1a6aeb7
Melvz <chuar@ph.ibm.com>
2018-04-16 15:18:20 +0800

    Create bank_account.py

---
+++ object-oriented-exercise/bank_account.py
@@ -0,0 +1,11 @@
+import datetime
+
+class Account(object):
+
+	def __init__(self, name=None, balance=0, number=None):
+		self.number = number
+		self.name = name
+		self.balance = balance
+		self.date_opened = datetime.datetime.now()
+		self.is_open = True
+
