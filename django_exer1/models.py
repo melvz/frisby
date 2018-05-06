c8cf2d0795fbcd32af9a46ad8524c7016e934d3f
Melvz <chuar@ph.ibm.com>
2018-05-06 16:13:31 +0800

    Create models.py

---
+++ django_exer1/models.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+
+class Food(models.Model):
+	name = models.CharField(verbose_name="Food Name", max_length=200)
+	calories = models.FloatField(verbose_name="Calories (KCAL)")
+	total_fat = models.FloatField(verbose_name="Total Fat (Gramos)")
+	saturated_fat = models.FloatField(verbose_name="Saturated FETS")
+	cholesterol = models.FloatField(verbose_name="Cholesterol")
+	sodium = models.FloatField(verbose_name="SODIUM")
+	total_carbs = models.FloatField(verbose_name="CARBS")
+	dietary_fiber = models.FloatField(verbose_name="fibre")
+	sugar = models.FloatField(verbose_name="sugar -- ")
+	protein = models.FloatField(verbose_name="protein")
+
+
+	def __str__(self):
+		return "%s" % self.name
+
+
+	
+
+
+class Meal(models.Model):
+	BREAKFAST = 1
+	MORNING_SNACK = 2
+	LUNCH = 3
+	AFTERNOON_SNACK = 4
+	DINNER = 5
+	EVENING_SNACK = 6
+
+	MEAL_TIME_TYPES = (
+		(BREAKFAST, "Breakfast"),
+		(MORNING_SNACK, "Morningsnack"),
+		(LUNCH, "lunch"),
+		(AFTERNOON_SNACK, "Afternoon snacker"),
+		(DINNER, "Dinner"),
+		(EVENING_SNACK, "Evening Snack")
+	)
+
+
+
+
+	food = models.ForeignKey(Food, verbose_name="Food")
+	serving_size = models.IntegerField(verbose_name="Service Size")
+	meal_time = models.IntegerField(verbose_name="Meal Time", choices=MEAL_TIME_TYPES)
+
+
+	def __str__(self):
+		return "%s" % self.food
+
+#	def get_total_calories(self):
+#		return self.serving_size * self.food.calories
+
+
