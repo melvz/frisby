e8f916dfb6076dfacbf7af239b18c4e9c032483d
Melvz <chuar@ph.ibm.com>
2018-05-06 16:15:57 +0800

    Create urls.py

---
+++ django_exer1/urls.py
@@ -0,0 +1,21 @@
+
+## This is located in  **/food_tracker/food/urls.py .
+
+from django.conf.urls import url
+
+
+
+
+from .views import index, add_meal, delete_meal, update_meal
+
+
+
+
+urlpatterns = [
+
+
+	url(r'^$', index, name='index'),
+	url(r'^add_meal$', add_meal, name='add-meal'),
+	url(r'^delete_meal/(?P<meal_id>\d+)$', delete_meal, name='delete-meal'),
+	url(r'^update_meal/(?P<meal_id>\d+)$', update_meal, name='update-meal'),
+]
